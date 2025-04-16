6de6837aeb445703aefcea2e29e5ee4882b28c3f
SeiRiN18 <olegsemonov90@gmail.com>
2025-04-16 23:06:09 +0300

    Realised lightweight mapper

---
+++ CustomMapper/CustomMapper/IMapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace CustomMapper
+{
+    public interface IMapper
+    {
+        TDestination Map<TSource, TDestination>(TSource source);
+    }
+}
---
+++ CustomMapper/CustomMapper/Mapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomMapper
+{
+    public class Mapper : IMapper
+    {
+        public TDestination Map<TSource, TDestination>(TSource source)
+        {
+            var mapFunc = MapperConfig.Get<TSource, TDestination>();
+            return mapFunc(source);
+        }
+    }
+}
